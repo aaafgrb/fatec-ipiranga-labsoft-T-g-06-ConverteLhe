d455dab531cb6032f84aa5f23e0bd84a1eb954e7
aaafgrb <lucasfuku77@gmail.com>
2024-08-19 12:09:01 -0300

    refactor composition string parsing
    refactor composition string parsing
    remove unnecessary files

--- src/arrow.erl
+++ src/arrow.erl
@@ -1,54 +1,97 @@
 -module(arrow).
--export([parseComposition/1, parseArrow/1, applyArguments/2, castArgumentType/1]).
--include("arrow.hrl").
+-export([parseComposition/1, resolveComposition/1]).
 -include("types.hrl").
 
-% receives a String (format: '|{functionName}/{type}{arguments}{0..n}|')
-% ex |sum/i1/i3| = 4
+% 'Tree' refered down isnt really a tree datatype. its more like a jagged list situation
 
-% escape characters with \
-
-% |func1/p1/p2|func2/p3|
-% ((func1 <- p1) <- p2) -> (func2 <- p3)
-
-% add other operators (like '|') for first and second function apply
-% example if $ is first -> $$ is first first -> expects {{_, _}, _}
-
-% mudar separadores? 
-%   \ e | nao funcionam por algum motivo
-
-% parse an arrow composition
+% parse an arrow composition string to a composition part tuple list
+%   expects rpn format
 parseComposition(Str) -> 
-  %escape characters after \
-  Escaped = escapeKeySymbols(Str),
+  L =  lists:flatten(string:replace(Str, "/", [0], all)),
+  LL = lists:flatten(string:replace(L, [92, 0], "/", all)),
+  lists:map(fun parseElement/1, string:tokens(LL, [0])).
 
-  %split by firt second and general splitters
-  %  put them in a tuple array
+parseElement([Prefix | Value]) -> P = prefixToType(Prefix), {P, convertToType(P, Value)};
+parseElement(_) -> [].
 
-  Split = string:tokens(Str, "|"),
-  fun(V) -> lists:foldl(fun(E, A) -> ?C_APPLY(E, A) end, V, [ parseArrow(X) || X <- Split]) end.
+prefixToType(36)  -> function; % $
+prefixToType(58)  -> store;    % :
+prefixToType(120) -> variable; % x
+prefixToType(115) -> string;   % s
+prefixToType(102) -> float;    % f
+prefixToType(105) -> integer;  % i
+prefixToType(T) -> throw({exception_unsupported_prefix, [T]}).
 
-escapeKeySymbols(Str) -> string:str(Str, "2").
+% not very safe
+convertToType(function, Value) -> Value;
+convertToType(string, Value)   -> Value;
+convertToType(variable, Value) -> try   string:to_integer(Value) 
+                                  of    { _, [] } -> [ X - 48 || X <- Value ];
+                                        _              -> throw({exception_convert_variable, Value})
+                                  catch error:_        -> throw({exception_convert_variable, Value})
+                                  end;
+convertToType(store, Value)    -> try   string:to_integer(Value) 
+                                  of    { _, [] } -> [ X - 48 || X <- Value ];
+                                        _              -> throw({exception_convert_store, Value})
+                                  catch error:_        -> throw({exception_convert_store, Value})
+                                  end;
+convertToType(float, Value)    -> try   string:to_float(Value) 
+                                  of    { Result, [] } -> Result;
+                                        _              -> throw({exception_convert_float, Value})
+                                  catch error:_        -> throw({exception_convert_float, Value})
+                                  end;
+convertToType(integer, Value)  -> try   string:to_integer(Value) 
+                                  of    { Result, [] } -> Result;
+                                        _              -> throw({exception_convert_integer, Value})
+                                  catch error:_        -> throw({exception_convert_integer, Value})
+                                  end;
+convertToType(T, _) -> throw({exception_unsupported_type, T}).
 
-% parse a single arrow
-parseArrow(Str) -> 
-  [Head | Tail] = string:tokens(Str, "/"),
+% --------------------------------------------------------------------------------
 
-  case lists:keyfind(Head, 1, ?ARROW_LIST) of
-    false     -> throw(?EX_UNSUPPORTED_ARROW);
-    {_, Func} -> applyArguments(Func, [ castArgumentType(X) || X <- Tail])
-  end.
+resolveComposition(Composition) -> fun(X) -> resolveComposition(Composition, [X], []) end.
 
-% applies recursively Args to the function Func
-applyArguments(Func, Args) -> lists:foldl(fun(E, A) -> A(E) end, Func, Args).
+resolveComposition([{string, Value}   |Tail], Tree, Stack) -> resolveComposition(Tail, Tree, [Value|Stack]);
+resolveComposition([{float, Value}    |Tail], Tree, Stack) -> resolveComposition(Tail, Tree, [Value|Stack]);
+resolveComposition([{integer, Value}  |Tail], Tree, Stack) -> resolveComposition(Tail, Tree, [Value|Stack]);
 
-% convert the argument string to the type it says it is (the first character)
-castArgumentType(Arg) -> 
-  case Arg of
-    []        -> throw(?EX_EMPTY_ARGUMENT);
-    [T | Str] -> 
-      case lists:keyfind(T, 1, ?TYPE_LIST) of
-        false     -> throw(?EX_UNSUPPORTED_TYPE);
-        {_, Func} -> Func(Str)
-      end
-  end.
+% remove or not remove the value from the stack?
+resolveComposition([{store, Value}    |Tail], Tree, [S | Stack]) -> 
+  resolveComposition(Tail, setTreeVariable(Tree, Value, S), [S|Stack]);
+
+resolveComposition([{variable, Value} |Tail], Tree, Stack) -> resolveComposition(Tail, Tree, [{variable, Value}|Stack]);
+
+resolveComposition([{function, Value} |Tail], Tree, Stack) -> resolveComposition(Tail, Tree, applyFunc(Value, Tree, Stack));
+
+resolveComposition([], T, S) -> {T, S}.
+
+applyFunc(Name, Tree, Stack) -> 
+  {Func, Arity} = funcList(Name),
+  {S, Rest} = try lists:split(Arity, Stack)
+                of    R -> R
+                catch error:badarg -> throw({exception_not_enough_parameters, Name})
+              end,
+  Params =    lists:map(fun(E) -> case E of
+                  {variable, V} -> getTreeVariable(Tree, V);
+                  V -> V
+                end
+              end, S),
+  [Func(Params)|Rest].
+
+getTreeVariable(Tree, [H|T]) -> getTreeVariable(lists:nth(H, Tree), T);
+getTreeVariable(Tree, []) -> Tree.
+
+% isnt using LCO
+setTreeVariable(Tree, [H|T], Value) -> 
+  {I, [_|II]} = lists:split(H - 1, Tree),
+  I ++ [setTreeVariable(lists:nth(H, Tree), T, Value) | II];
+setTreeVariable(_, [], Value) -> Value.
+
+
+% funcList( {func name} ) -> ( {func} , {arity} )
+funcList("sum")      -> { fun([F, S]) -> F + S end, 2 };
+funcList("multiply") -> { fun([F, S]) -> F * S end, 2 };
+funcList("divide")   -> { fun([F, S]) -> F / S end, 2 };
+funcList("clone")    -> { fun([N, F]) -> lists:duplicate(N, F) end, 2 };
+funcList("concat")   -> { fun([F, S]) -> F ++ S end, 2 };
+funcList(F) -> throw({exception_unsupported_function, F}).
--- src/arrowTest.erl
+++ src/arrowTest.erl
@@ -2,59 +2,38 @@
 -export([start/0]).
 -include_lib("eunit/include/eunit.hrl").
 -include("throws.hrl").
--import(arrow, [parseArrow/1, applyArguments/2, castArgumentType/1, parseComposition/1]).
+-import(arrow, [parseComposition/1, resolveComposition/1]).
 
 % for manual testing -----------------------------------------
-start() -> castArgumentType("ii").
+start() -> ok.
 
 % run tests with {this module}:test() ------------------------------------------
 
-parseComposition1_test() -> T = parseComposition("|sum/i3|sum/f9.2"), ?assertEqual(13.2, T(1)).
-parseComposition2_test() -> T = parseComposition("|concatAfter/i3|"), ?assertEqual("asdi3", T("asd")).
+parseComposition_typeConversion_test() -> 
+  C = parseComposition("sabc/i123/f3.2/$concat/:123/x12"), 
+  ?assertEqual([{string, "abc"}, {integer, 123}, {float, 3.2}, {function, "concat"}, {store, [1,2,3]}, {variable, [1,2]}], C).
 
-% ------------------------------------------------------------------------------
+parseComposition_forwardSlashEscape_test() -> 
+  C = parseComposition("s\\/!@#$%^&*()-+=,.<>[]{}'\"\\あ 間ç"), 
+  ?assertEqual([{string, "/!@#$%^&*()-+=,.<>[]{}'\"\\あ 間ç"}], C).
+
+parseComposition_typeConvertError1_test() -> ?assertThrow({exception_convert_integer, "abc"}, parseComposition("iabc")).
+parseComposition_typeConvertError2_test() -> ?assertThrow({exception_convert_float, "abc"}, parseComposition("fabc")).
+parseComposition_typeConvertError3_test() -> ?assertThrow({exception_convert_float, "3"}, parseComposition("f3")).
+parseComposition_typeConvertError4_test() -> ?assertThrow({exception_convert_store, "a123"}, parseComposition(":a123")).
+parseComposition_typeConvertError5_test() -> ?assertThrow({exception_convert_variable, "a123"}, parseComposition("xa123")).
+
+parseComposition_prefixError_test() -> ?assertThrow({exception_unsupported_prefix, "a"}, parseComposition("a123")).
 
 
-% sum
-parseArrow1sum_test() -> ?assertEqual(5, parseArrow("sum/i2/i3")).
-parseArrow2sum_test() -> ?assertEqual(5, parseArrow("sum/i2/i3/")).
-parseArrow3sum_test() -> ?assertEqual(6.0, parseArrow("sum/f2.5/f3.5")).
-parseArrow4sum_test() -> ?assertEqual(5.5, parseArrow("sum/f2.5/i3")).
-parseArrow5sum_test() -> ?assertThrow(?EX_UNSUPPORTED_TYPE, parseArrow("sum/f2.5/3")).
-parseArrow6sum_test() -> ?assertError(badarith, parseArrow("sum/f2.5/sasd")).
-parseArrow7sum_test() -> ?assertEqual(5, parseArrow("sum/i2//i3")).
+resolveComposition_resolve_test() -> 
+  C = resolveComposition(parseComposition("i1/f2.4/$sum")), 
+  ?assertEqual({[1], [3.4]}, C(1)).
 
-% concatAfter
-parseArrow1concatAfter_test() -> ?assertEqual("i3i2", parseArrow("concatAfter/si2/si3")).
-parseArrow2concatAfter_test() -> ?assertEqual("i3i/2", parseArrow("concatAfter/si\\/2/si3")).
+resolveComposition_functionNotFound_test() -> 
+  C = resolveComposition(parseComposition("i1/f2.4/$hnuiads")), 
+  ?assertThrow({exception_unsupported_function, "hnuiads"}, C(1)).
 
-% -----------------------------------------------------------------------------
-
-applyArguments1_test() -> ?assertEqual(1, applyArguments(fun(F) -> F end, [1])).
-applyArguments2_test() -> ?assertError({badfun, _}, applyArguments(fun(F) -> F end, [1, 1])).
-applyArguments3_test() -> ?assertEqual(3, applyArguments(fun(F) -> fun(S) -> F + S end end, [1, 2])).
-applyArguments4_test() -> ?assertError(badarith, applyArguments(fun(F) -> fun(S) -> F + S end end, [1, "a"])).
-
-% -----------------------------------------------------------------------------
-
-castArgumentTypei1_test() -> ?assertEqual(0, castArgumentType("i0")).
-castArgumentTypei2_test() -> ?assertEqual(-0, castArgumentType("i-0")).
-castArgumentTypei3_test() -> ?assertEqual(-10, castArgumentType("i-10")).
-castArgumentTypei4_test() -> ?assertEqual(4123124512312312341231245123123123, castArgumentType("i4123124512312312341231245123123123")).
-castArgumentTypei5_test() -> ?assertThrow(?EX_UNSUPPORTED_TYPE, castArgumentType("10")).
-castArgumentTypei6_test() -> ?assertThrow(?EX_TYPE_PARSE_ERROR, castArgumentType("ii")).
-castArgumentTypei7_test() -> ?assertThrow(?EX_EMPTY_ARGUMENT, castArgumentType("")).
-
-castArgumentTypef1_test() -> ?assertEqual(0.0, castArgumentType("f0.0")).
-castArgumentTypef2_test() -> ?assertEqual(-0.0, castArgumentType("f-0.0")).
-castArgumentTypef3_test() -> ?assertEqual(-10.3, castArgumentType("f-10.3")).
-castArgumentTypef4_test() -> ?assertEqual(4123124512312312341231245123123123.123123123, castArgumentType("f4123124512312312341231245123123123.123123123")).
-castArgumentTypef5_test() -> ?assertThrow(?EX_UNSUPPORTED_TYPE, castArgumentType("10.0")).
-castArgumentTypef6_test() -> ?assertThrow(?EX_TYPE_PARSE_ERROR, castArgumentType("fi")).
-castArgumentTypef7_test() -> ?assertThrow(?EX_TYPE_PARSE_ERROR, castArgumentType("f0")).
-
-castArgumentTypes1_test() -> ?assertEqual("0123", castArgumentType("s0123")).
-castArgumentTypes2_test() -> ?assertEqual("isd", castArgumentType("sisd")).
-castArgumentTypes3_test() -> ?assertThrow(?EX_UNSUPPORTED_TYPE, castArgumentType("10f")).
-castArgumentTypes4_test() -> ?assertEqual("!@#$%^&*()_+{}[]-=|\'\"/,.<>", castArgumentType("s!@#$%^&*()_+{}[]-=|\'\"/,.<>")).
-
+resolveComposition_notEnoughParameters_test() -> 
+  C = resolveComposition(parseComposition("f2.4/$sum")), 
+  ?assertThrow({exception_not_enough_parameters, "sum"}, C(1)).
